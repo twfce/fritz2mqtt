ec66dc669b9c71102da3abbc8e321176450f3424
twfce <me@2fce.de>
2020-11-23 22:13:20 +0100

    Update

--- main.py
+++ main.py
@@ -8,6 +8,16 @@ from colored import fg, attr
 from fritzconnection.lib.fritzstatus import FritzStatus
 import paho.mqtt.client as mqtt
 
+fritzPassword = os.environ["FRITZBOX_PASSWORD"]
+fritzIP = os.environ["FRITZBOX_IP"]
+
+mqttBroker = os.environ["MQTT_BROKER"]
+mqttUsername = os.environ["MQTT_USERNAME"]
+mqttPassword = os.environ["MQTT_PASSWORD"]
+mqttBaseTopic = os.environ["MQTT_BASE_TOPIC"] if "MQTT_BASE_TOPIC" in os.environ else "fritzBox"
+mqttPort = os.environ["MQTT_PORT"] if "MQTT_PORT" in os.environ else 1883
+sleepTime = int(os.environ["FRITZ2MQTT_SLEEP"]) if "FRITZ2MQTT_SLEEP" in os.environ else 10
+
 def getFritzBoxReport(ip, password):
     print ("[~] Connecting to Fritz.Box ({ip})".format(ip=ip))
     fritzStatus = FritzStatus(address=fritzIP, password=fritzPassword)
@@ -33,7 +43,6 @@ def getFritzBoxReport(ip, password):
 def onMQTTConnect(client, userdata, flags, rc):
     if rc == 0:
         print ("{color}[+] Connected to MQTT broker (Return code: {rc}){reset}".format(color=fg(2), rc=rc, reset=attr(0)))
-        client.publish("/fritzbox", "Hello World! Status reporter is running.")
     else:
         print ("{color}[-] Not connected to MQTT broker (Return code: {rc}){reset}".format(color=fg(1), rc=rc, reset=attr(0)))
 
@@ -41,27 +50,19 @@ def onMQTTConnect(client, userdata, flags, rc):
 def onMQTTPublish(client, userdata, mid):
     print ("{color}[*] Published to MQTT broker (Message ID: {mid}){reset}".format(color=fg(135), mid=mid, reset=attr(0)))
 
-fritzPassword = os.environ["FRITZBOX_PASSWORD"]
-fritzIP = os.environ["FRITZBOX_IP"]
+def main():
+    client = mqtt.Client()
+    client.on_connect = onMQTTConnect
+    client.on_publish = onMQTTPublish
+    client.username_pw_set(username=mqttUsername, password=mqttPassword)
+    client.connect(mqttBroker, mqttPort, 60)
+    client.loop_start()
 
-mqttBroker = os.environ["MQTT_BROKER"]
-mqttUsername = os.environ["MQTT_USERNAME"]
-mqttPassword = os.environ["MQTT_PASSWORD"]
-if "MQTT_PORT" in os.environ:
-    mqttPort = os.environ["MQTT_PORT"]
-else:
-    mqttPort = 1883
+    while True:
+        report = getFritzBoxReport(fritzIP, fritzPassword)
+        print (json.dumps(report, indent=1))
+        client.publish("{base}/report".format(base = mqttBaseTopic), json.dumps(report))
+        sleep(sleepTime)
 
-
-client = mqtt.Client()
-client.on_connect = onMQTTConnect
-client.on_publish = onMQTTPublish
-client.username_pw_set(username=mqttUsername, password=mqttPassword)
-client.connect(mqttBroker, mqttPort, 60)
-client.loop_start()
-
-while True:
-    report = getFritzBoxReport(fritzIP, fritzPassword)
-    print (json.dumps(report, indent=1))
-    client.publish("/fritzbox/report", json.dumps(report))
-    sleep(60)
+if __name__ == "__main__":
+    main()
